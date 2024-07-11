05fead09044cd7aa759911a3fc9fce0aad72e7df
Shwetavishwase <147078113+Shwetavishwase@users.noreply.github.com>
2024-07-11 17:16:03 +0530

    Add files via upload

---
+++ AccessModifersAdv/src/main/java/com/csi/core/AccessModifers.java
@@ -0,0 +1,29 @@
+package com.csi.core;
+
+class Product {
+	private int productId=101;
+
+	String productName="APPLE PHONE";
+	
+	double productPrice=989898.22;
+	
+	public int getProductId()
+	{
+		return productId;
+	}
+	
+	public void setProductId(int productId)
+	{
+		this.productId=productId;
+	}
+}
+
+public class AccessModifers {
+
+	public static void main(String[] args) {
+		
+		Product product=new Product();
+		
+		System.out.println(""+ product.getProductId() + ""+product.productName+""+product.productPrice);
+	}
+}
---
+++ AccessModifersAdv/src/main/java/com/csi/core/DefaultConcept.java
@@ -0,0 +1,11 @@
+package com.csi.core;
+
+public class DefaultConcept {
+
+	public static void main(String[] args) {
+		
+		Product product=new Product();
+		
+		System.out.println(" "+ product.productName);
+	}
+}
